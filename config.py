a2a9b77a6604164da30b1d081e5dfdab905f2555
Vaithish-techie <vaithishsrineel@gmail.com>
2025-02-28 23:40:27 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,8 +6,8 @@ load_dotenv()
 SECRET_KEY = os.getenv("SECRET_KEY")
 SQLALCHEMY_DATABASE_URI = os.getenv("SQLALCHEMY_DATABASE_URI")
 JUDGE0_API_KEY = os.getenv("JUDGE0_API_KEY")
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 JUDGE0_BASE_URL = "https://judge0-ce.p.rapidapi.com/submissions"
+HF_TOKEN = os.getenv("HF_TOKEN")
 # Supported Programming Languages (Judge0 API)
 LANGUAGE_MAP = {
     "python": 71,
